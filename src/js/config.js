5c2d384c8a9bae003a2625c9f3a0ba0bb10648e5
MoneaAdrianMarius <marius.monea89@gmail.com>
2021-12-12 16:04:40 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd938fe28-ba66-4df0-94c5-1a262bf4af6b';
+export const MODAL_CLOSE_SEC = 2.5;
