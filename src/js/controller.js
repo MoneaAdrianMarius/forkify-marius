ad2a1dd24e46f7bcbe8cb8bf2722c527cea64e83
MoneaAdrianMarius <marius.monea89@gmail.com>
2021-12-12 16:45:19 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,6 +119,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Wlecome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -127,6 +131,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
