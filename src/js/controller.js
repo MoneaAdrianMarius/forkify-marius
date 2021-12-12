f76313a4eb4273fe0b04256cbb281816f302b562
MoneaAdrianMarius <marius.monea89@gmail.com>
2021-12-12 17:43:48 +0200

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,10 +119,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Wlecome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -131,6 +127,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
